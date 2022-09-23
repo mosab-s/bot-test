be85bf3bb2db46b07407b9c32092f450dde94126
mosab-s <55572618+mosab-s@users.noreply.github.com>
2022-09-23 20:29:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 import discord
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("We have logged in as {0.user}".foramt(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith("$hello"):
+        await message.channel.send("Hello!")
+
+client.run(MTAyMjkxNTE1MzM4NzMzNTc4NA.GsKDox.An1vhO2ecmJbTl0vtDPWE3yzLl-eFQx2ChBRcI)
